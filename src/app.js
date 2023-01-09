2d246c2585cac8e1abd5418135c3c11d9f59caf8
martadudka <martakryklevych@gmail.com>
2023-01-09 16:02:30 +0100

    Integrated API + function displayWeather

---
+++ src/app.js
@@ -0,0 +1,15 @@
+function displayWeather(response) {
+let degreesElement = document.querySelector("#degrees");
+degreesElement.innerHTML = Math.round(response.data.temperature.current);
+
+let cityNameElement = document.querySelector("#city-name");
+cityNameElement.innerHTML = response.data.city;
+
+let weatherDescriptionElement = document.querySelector("#weather-description");
+weatherDescriptionElement.innerHTML = response.data.condition.description;
+}
+
+let apiKey = "1d1a4ta9d508e3cb925a520dd24fdc3o";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
